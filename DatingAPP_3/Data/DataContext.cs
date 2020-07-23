fc977612f78d7b66ff90a384921aa7c3ad62891c
unknown <aleixripoll92@gmail.com>
2020-07-23 10:06:17 +0200

    initial commit

---
+++ DatingAPP_3/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using DatingAPP_3.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingAPP_3.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
